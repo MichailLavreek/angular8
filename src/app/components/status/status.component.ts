d03da39fe34eb8ecdfa2cea0e7bfcdcedcf6605c
Sergey <sergey.morenets@yandex.ua>
2019-03-24 12:11:24 +0200

    Task 14. Router events

--- src/app/components/status/status.component.ts
+++ src/app/components/status/status.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {AsyncEventBus} from '../../core/async-event-bus';
-import {map} from 'rxjs/operators';
+import {filter, map} from 'rxjs/operators';
+import {NavigationEnd, Router, RouterEvent} from '@angular/router';
 
 @Component({
   selector: 'app-status',
@@ -12,9 +13,11 @@ export class StatusComponent implements OnInit {
 
   status$: Observable<string>;
 
-  constructor(private asyncEventBus: AsyncEventBus) {
-    this.status$ = this.asyncEventBus.subject.pipe(
-      map(event => event.message));
+  constructor(private router: Router) {
+    this.status$ = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map((event: RouterEvent) => event.url)
+    );
   }
 
   ngOnInit() {
